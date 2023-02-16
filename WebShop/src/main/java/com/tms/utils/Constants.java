050ba2a00246809b63b76e273492628e730a0b82
natariyz <evgeniy.vabishevich.99@mail.ru>
2023-02-16 23:03:51 +0300

    Made private constructor for Constants class + small fix

--- WebShop/src/main/java/com/tms/utils/Constants.java
+++ WebShop/src/main/java/com/tms/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.tms.utils;
 
 public class Constants {
+    private Constants(){}
     public static final String USER_LOGIN = "admin";
     public static final String USER_PASSWORD = "admin";
 }
